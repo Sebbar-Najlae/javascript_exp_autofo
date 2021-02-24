b3d8aa53bebdb212817e0c4812aedd59c01d0ead
sebbar najlae <sebbar.najlae.dev@gmail.com>
2021-02-24 17:06:05 +0100

    commit loop

--- autoformation/home.js
+++ autoformation/home.js
@@ -99,7 +99,21 @@ for (let num = 0; num <= 100; num++) {
 }
 
 */
+// loop control
+var x = 1;
+document.write("Entering the loop<br /> ");
 
+while (x < 10)
+{
+   x = x + 1;
+   
+   if (x == 5) {
+      continue; // skip rest of the loop body
+   }
+   document.write( x + "<br />");
+}
+
+document.write("Exiting the loop!<br /> ");
 // Data types 
 
 let yourAge = 18; //number
